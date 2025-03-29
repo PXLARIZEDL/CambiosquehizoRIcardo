20af58beffeec94edb05377713d488b238ceff80
PXLARIZEDL <ricardopimentelm3i@gmail.com>
2025-03-29 14:14:12 -0400

    cambio ramon 3/23/25

--- Capa_Presentacion_Proyecto_Final/FrmsControldeempleados/FrmVisualregistrarempleado .Designer.cs
+++ Capa_Presentacion_Proyecto_Final/FrmsControldeempleados/FrmVisualregistrarempleado .Designer.cs
@@ -54,16 +54,18 @@
             label6 = new Label();
             TXT_ID_GENERO = new TextBox();
             label7 = new Label();
+            cb_direccion = new ComboBox();
+            cb_genero = new ComboBox();
             SuspendLayout();
             // 
             // LBL_TITULO
             // 
             LBL_TITULO.AutoSize = true;
             LBL_TITULO.Font = new Font("Segoe UI", 24F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            LBL_TITULO.Location = new Point(238, 90);
+            LBL_TITULO.Location = new Point(208, 68);
             LBL_TITULO.Name = "LBL_TITULO";
             LBL_TITULO.RightToLeft = RightToLeft.Yes;
-            LBL_TITULO.Size = new Size(521, 54);
+            LBL_TITULO.Size = new Size(422, 45);
             LBL_TITULO.TabIndex = 39;
             LBL_TITULO.Text = "REGISTRO DE EMPLEADOS";
             // 
@@ -71,10 +73,9 @@
             // 
             BTN_ELIMINAR.BackColor = Color.Crimson;
             BTN_ELIMINAR.Font = new Font("Segoe UI", 15.75F, FontStyle.Bold);
-            BTN_ELIMINAR.Location = new Point(526, 614);
-            BTN_ELIMINAR.Margin = new Padding(3, 4, 3, 4);
+            BTN_ELIMINAR.Location = new Point(460, 460);
             BTN_ELIMINAR.Name = "BTN_ELIMINAR";
-            BTN_ELIMINAR.Size = new Size(194, 100);
+            BTN_ELIMINAR.Size = new Size(170, 75);
             BTN_ELIMINAR.TabIndex = 38;
             BTN_ELIMINAR.Text = "CANCELAR";
             BTN_ELIMINAR.UseVisualStyleBackColor = false;
@@ -83,10 +84,9 @@
             // 
             BTN_GUARDAR.BackColor = SystemColors.HotTrack;
             BTN_GUARDAR.Font = new Font("Segoe UI", 15.75F, FontStyle.Bold);
-            BTN_GUARDAR.Location = new Point(238, 614);
-            BTN_GUARDAR.Margin = new Padding(3, 4, 3, 4);
+            BTN_GUARDAR.Location = new Point(208, 460);
             BTN_GUARDAR.Name = "BTN_GUARDAR";
-            BTN_GUARDAR.Size = new Size(203, 100);
+            BTN_GUARDAR.Size = new Size(178, 75);
             BTN_GUARDAR.TabIndex = 37;
             BTN_GUARDAR.Text = "REGISTRAR";
             BTN_GUARDAR.UseVisualStyleBackColor = false;
@@ -96,61 +96,56 @@
             // 
             LBL_CARGO.AutoSize = true;
             LBL_CARGO.Font = new Font("Segoe UI", 11.25F, FontStyle.Bold);
-            LBL_CARGO.Location = new Point(237, 522);
+            LBL_CARGO.Location = new Point(207, 392);
             LBL_CARGO.Name = "LBL_CARGO";
-            LBL_CARGO.Size = new Size(77, 25);
+            LBL_CARGO.Size = new Size(61, 20);
             LBL_CARGO.TabIndex = 35;
             LBL_CARGO.Text = "CARGO";
             // 
             // cbCargo
             // 
             cbCargo.FormattingEnabled = true;
-            cbCargo.Location = new Point(354, 524);
-            cbCargo.Margin = new Padding(3, 4, 3, 4);
+            cbCargo.Location = new Point(310, 393);
             cbCargo.Name = "cbCargo";
-            cbCargo.Size = new Size(161, 28);
+            cbCargo.Size = new Size(141, 23);
             cbCargo.TabIndex = 34;
             // 
             // TXT_EDAD
             // 
-            TXT_EDAD.Location = new Point(354, 286);
-            TXT_EDAD.Margin = new Padding(3, 4, 3, 4);
+            TXT_EDAD.Location = new Point(310, 214);
             TXT_EDAD.Name = "TXT_EDAD";
-            TXT_EDAD.Size = new Size(161, 27);
+            TXT_EDAD.Size = new Size(141, 23);
             TXT_EDAD.TabIndex = 33;
             TXT_EDAD.TextChanged += TXT_EDAD_TextChanged;
             // 
             // TXT_APELLIDO
             // 
-            TXT_APELLIDO.Location = new Point(354, 220);
-            TXT_APELLIDO.Margin = new Padding(3, 4, 3, 4);
+            TXT_APELLIDO.Location = new Point(310, 165);
             TXT_APELLIDO.Name = "TXT_APELLIDO";
-            TXT_APELLIDO.Size = new Size(161, 27);
+            TXT_APELLIDO.Size = new Size(141, 23);
             TXT_APELLIDO.TabIndex = 32;
             // 
             // TXT_SUELDO
             // 
-            TXT_SUELDO.Location = new Point(354, 342);
-            TXT_SUELDO.Margin = new Padding(3, 4, 3, 4);
+            TXT_SUELDO.Location = new Point(310, 256);
             TXT_SUELDO.Name = "TXT_SUELDO";
-            TXT_SUELDO.Size = new Size(161, 27);
+            TXT_SUELDO.Size = new Size(141, 23);
             TXT_SUELDO.TabIndex = 31;
             // 
             // TXT_NOMBRE
             // 
-            TXT_NOMBRE.Location = new Point(354, 174);
-            TXT_NOMBRE.Margin = new Padding(3, 4, 3, 4);
+            TXT_NOMBRE.Location = new Point(310, 130);
             TXT_NOMBRE.Name = "TXT_NOMBRE";
-            TXT_NOMBRE.Size = new Size(161, 27);
+            TXT_NOMBRE.Size = new Size(141, 23);
             TXT_NOMBRE.TabIndex = 30;
             // 
             // LBL_SUELDO
             // 
             LBL_SUELDO.AutoSize = true;
             LBL_SUELDO.Font = new Font("Segoe UI", 11.25F, FontStyle.Bold);
-            LBL_SUELDO.Location = new Point(207, 342);
+            LBL_SUELDO.Location = new Point(181, 256);
             LBL_SUELDO.Name = "LBL_SUELDO";
-            LBL_SUELDO.Size = new Size(85, 25);
+            LBL_SUELDO.Size = new Size(66, 20);
             LBL_SUELDO.TabIndex = 29;
             LBL_SUELDO.Text = "SUELDO";
             // 
@@ -158,9 +153,9 @@
             // 
             LBL_APELLIDO.AutoSize = true;
             LBL_APELLIDO.Font = new Font("Segoe UI", 11.25F, FontStyle.Bold);
-            LBL_APELLIDO.Location = new Point(207, 224);
+            LBL_APELLIDO.Location = new Point(181, 168);
             LBL_APELLIDO.Name = "LBL_APELLIDO";
-            LBL_APELLIDO.Size = new Size(101, 25);
+            LBL_APELLIDO.Size = new Size(80, 20);
             LBL_APELLIDO.TabIndex = 28;
             LBL_APELLIDO.Text = "APELLIDO";
             // 
@@ -168,10 +163,10 @@
             // 
             LBL_NOMBRE.AutoSize = true;
             LBL_NOMBRE.Font = new Font("Segoe UI", 11.25F, FontStyle.Bold);
-            LBL_NOMBRE.Location = new Point(214, 178);
+            LBL_NOMBRE.Location = new Point(187, 134);
             LBL_NOMBRE.Name = "LBL_NOMBRE";
             LBL_NOMBRE.RightToLeft = RightToLeft.Yes;
-            LBL_NOMBRE.Size = new Size(93, 25);
+            LBL_NOMBRE.Size = new Size(74, 20);
             LBL_NOMBRE.TabIndex = 27;
             LBL_NOMBRE.Text = "NOMBRE";
             // 
@@ -179,69 +174,63 @@
             // 
             LBL_EDAD.AutoSize = true;
             LBL_EDAD.Font = new Font("Segoe UI", 11.25F, FontStyle.Bold);
-            LBL_EDAD.Location = new Point(207, 286);
+            LBL_EDAD.Location = new Point(181, 214);
             LBL_EDAD.Name = "LBL_EDAD";
-            LBL_EDAD.Size = new Size(63, 25);
+            LBL_EDAD.Size = new Size(50, 20);
             LBL_EDAD.TabIndex = 40;
             LBL_EDAD.Text = "EDAD";
             // 
             // TXT_FECHAINGRESO
             // 
-            TXT_FECHAINGRESO.Location = new Point(354, 404);
-            TXT_FECHAINGRESO.Margin = new Padding(3, 4, 3, 4);
+            TXT_FECHAINGRESO.Location = new Point(310, 303);
             TXT_FECHAINGRESO.Name = "TXT_FECHAINGRESO";
-            TXT_FECHAINGRESO.Size = new Size(161, 27);
+            TXT_FECHAINGRESO.Size = new Size(141, 23);
             TXT_FECHAINGRESO.TabIndex = 41;
             // 
             // cbGenero
             // 
             cbGenero.FormattingEnabled = true;
-            cbGenero.Location = new Point(354, 460);
-            cbGenero.Margin = new Padding(3, 4, 3, 4);
+            cbGenero.Location = new Point(310, 345);
             cbGenero.Name = "cbGenero";
-            cbGenero.Size = new Size(161, 28);
+            cbGenero.Size = new Size(141, 23);
             cbGenero.TabIndex = 42;
             // 
             // textBox1
             // 
-            textBox1.Location = new Point(711, 332);
-            textBox1.Margin = new Padding(3, 4, 3, 4);
+            textBox1.Location = new Point(622, 249);
             textBox1.Name = "textBox1";
-            textBox1.Size = new Size(161, 27);
+            textBox1.Size = new Size(141, 23);
             textBox1.TabIndex = 43;
             // 
             // TXT_CEDULA
             // 
-            TXT_CEDULA.Location = new Point(711, 244);
-            TXT_CEDULA.Margin = new Padding(3, 4, 3, 4);
+            TXT_CEDULA.Location = new Point(622, 183);
             TXT_CEDULA.Name = "TXT_CEDULA";
-            TXT_CEDULA.Size = new Size(161, 27);
+            TXT_CEDULA.Size = new Size(141, 23);
             TXT_CEDULA.TabIndex = 44;
             // 
             // TXT_TELEFONO
             // 
-            TXT_TELEFONO.Location = new Point(711, 174);
-            TXT_TELEFONO.Margin = new Padding(3, 4, 3, 4);
+            TXT_TELEFONO.Location = new Point(622, 130);
             TXT_TELEFONO.Name = "TXT_TELEFONO";
-            TXT_TELEFONO.Size = new Size(161, 27);
+            TXT_TELEFONO.Size = new Size(141, 23);
             TXT_TELEFONO.TabIndex = 45;
             // 
             // TXT_ID_DIRECCION
             // 
-            TXT_ID_DIRECCION.Location = new Point(711, 480);
-            TXT_ID_DIRECCION.Margin = new Padding(3, 4, 3, 4);
+            TXT_ID_DIRECCION.Location = new Point(23, 45);
             TXT_ID_DIRECCION.Name = "TXT_ID_DIRECCION";
-            TXT_ID_DIRECCION.Size = new Size(161, 27);
+            TXT_ID_DIRECCION.Size = new Size(141, 23);
             TXT_ID_DIRECCION.TabIndex = 47;
             // 
             // label1
             // 
             label1.AutoSize = true;
             label1.Font = new Font("Segoe UI", 11.25F, FontStyle.Bold);
-            label1.Location = new Point(563, 172);
+            label1.Location = new Point(493, 129);
             label1.Name = "label1";
             label1.RightToLeft = RightToLeft.Yes;
-            label1.Size = new Size(106, 25);
+            label1.Size = new Size(84, 20);
             label1.TabIndex = 49;
             label1.Text = "TELEFONO";
             // 
@@ -249,10 +238,10 @@
             // 
             label2.AutoSize = true;
             label2.Font = new Font("Segoe UI", 11.25F, FontStyle.Bold);
-            label2.Location = new Point(563, 248);
+            label2.Location = new Point(493, 186);
             label2.Name = "label2";
             label2.RightToLeft = RightToLeft.Yes;
-            label2.Size = new Size(85, 25);
+            label2.Size = new Size(67, 20);
             label2.TabIndex = 50;
             label2.Text = "CEDULA";
             // 
@@ -260,10 +249,10 @@
             // 
             label4.AutoSize = true;
             label4.Font = new Font("Segoe UI", 11.25F, FontStyle.Bold);
-            label4.Location = new Point(563, 484);
+            label4.Location = new Point(493, 363);
             label4.Name = "label4";
             label4.RightToLeft = RightToLeft.Yes;
-            label4.Size = new Size(112, 25);
+            label4.Size = new Size(89, 20);
             label4.TabIndex = 52;
             label4.Text = "DIRECCION";
             // 
@@ -271,10 +260,10 @@
             // 
             label5.AutoSize = true;
             label5.Font = new Font("Segoe UI", 11.25F, FontStyle.Bold);
-            label5.Location = new Point(563, 336);
+            label5.Location = new Point(493, 252);
             label5.Name = "label5";
             label5.RightToLeft = RightToLeft.Yes;
-            label5.Size = new Size(106, 25);
+            label5.Size = new Size(84, 20);
             label5.TabIndex = 53;
             label5.Text = "TELEFONO";
             // 
@@ -282,36 +271,54 @@
             // 
             label6.AutoSize = true;
             label6.Font = new Font("Segoe UI", 11.25F, FontStyle.Bold);
-            label6.Location = new Point(563, 528);
+            label6.Location = new Point(493, 396);
             label6.Name = "label6";
             label6.RightToLeft = RightToLeft.Yes;
-            label6.Size = new Size(112, 25);
+            label6.Size = new Size(89, 20);
             label6.TabIndex = 54;
             label6.Text = "ID GENERO";
             // 
             // TXT_ID_GENERO
             // 
-            TXT_ID_GENERO.Location = new Point(711, 529);
-            TXT_ID_GENERO.Margin = new Padding(3, 4, 3, 4);
+            TXT_ID_GENERO.Location = new Point(23, 82);
             TXT_ID_GENERO.Name = "TXT_ID_GENERO";
-            TXT_ID_GENERO.Size = new Size(161, 27);
+            TXT_ID_GENERO.Size = new Size(141, 23);
             TXT_ID_GENERO.TabIndex = 55;
             // 
             // label7
             // 
             label7.AutoSize = true;
             label7.Font = new Font("Segoe UI", 11.25F, FontStyle.Bold);
-            label7.Location = new Point(207, 408);
+            label7.Location = new Point(181, 306);
             label7.Name = "label7";
-            label7.Size = new Size(135, 25);
+            label7.Size = new Size(106, 20);
             label7.TabIndex = 56;
             label7.Text = "Fecha Ingreso";
             // 
+            // cb_direccion
+            // 
+            cb_direccion.FormattingEnabled = true;
+            cb_direccion.Location = new Point(631, 360);
+            cb_direccion.Name = "cb_direccion";
+            cb_direccion.Size = new Size(141, 23);
+            cb_direccion.TabIndex = 57;
+            cb_direccion.SelectedIndexChanged += cb_direccion_SelectedIndexChanged;
+            // 
+            // cb_genero
+            // 
+            cb_genero.FormattingEnabled = true;
+            cb_genero.Location = new Point(631, 397);
+            cb_genero.Name = "cb_genero";
+            cb_genero.Size = new Size(141, 23);
+            cb_genero.TabIndex = 58;
+            // 
             // FrmVisualregistrarempleado
             // 
-            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(1090, 840);
+            ClientSize = new Size(954, 630);
+            Controls.Add(cb_genero);
+            Controls.Add(cb_direccion);
             Controls.Add(label7);
             Controls.Add(TXT_ID_GENERO);
             Controls.Add(label6);
@@ -338,7 +345,6 @@
             Controls.Add(LBL_SUELDO);
             Controls.Add(LBL_APELLIDO);
             Controls.Add(LBL_NOMBRE);
-            Margin = new Padding(3, 4, 3, 4);
             Name = "FrmVisualregistrarempleado";
             Text = "FrmVisualregistrarempleado";
             Load += FrmVisualregistrarempleado_Load;
@@ -374,5 +380,7 @@
         private Label label6;
         private TextBox TXT_ID_GENERO;
         private Label label7;
+        private ComboBox cb_direccion;
+        private ComboBox cb_genero;
     }
 }
--- Capa_Presentacion_Proyecto_Final/FrmsControldeempleados/FrmVisualregistrarempleado .cs
+++ Capa_Presentacion_Proyecto_Final/FrmsControldeempleados/FrmVisualregistrarempleado .cs
@@ -12,10 +12,12 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using Capa_Negocio;
 using Capa_Negocio.Interfaces;
+using System.Drawing.Text;
+using System.Linq.Expressions;
 namespace Capa_Presentacion_Proyecto_Final
 {
-    
-public partial class FrmVisualregistrarempleado : Form
+
+    public partial class FrmVisualregistrarempleado : Form
     {
         public FrmVisualregistrarempleado()
         {
@@ -24,19 +26,68 @@ public partial class FrmVisualregistrarempleado : Form
 
         private void BTN_GUARDAR_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("HOLA MENOL");
             CD_Empleados empleados = new CD_Empleados();
+
             try
             {
-                empleados.Insertar(TXT_NOMBRE.Text, TXT_APELLIDO.Text, TXT_CEDULA.Text, "", "", cbCargo.Text, TXT_TELEFONO.Text, TXT_FECHAINGRESO.Text, "", TXT_ID_DIRECCION.Text, cbGenero.Text);
+                DateTime fechaIngreso;
+                if (!DateTime.TryParse(TXT_FECHAINGRESO.Text, out fechaIngreso))
+                {
+                    MessageBox.Show("Fecha de ingreso no válida");
+                    return;
+                }
+
+                // Verificar que el ID de la dirección sea válido (número)
+              /*  int idDireccion;
+                if (!int.TryParse(TXT_ID_DIRECCION.Text, out idDireccion))
+                {
+                    MessageBox.Show("El ID de dirección no es válido");
+                    return;
+                }
+
+                // Asegurarse de que el ID_DIRECCION exista en la base de datos
+                if (!VerificarDireccionExistente1(idDireccion))
+                {
+                    MessageBox.Show("La dirección no existe. Debe insertar primero la dirección.");
+                    return;
+                }*/
+
+                int idGenero;
+                if (!int.TryParse(cbGenero.Text, out idGenero))
+                {
+                    MessageBox.Show("El ID de género no es válido");
+                    return;
+                }
+
+                // Insertar el empleado con el ID de dirección válido
+               /* empleados.Insertar(TXT_NOMBRE.Text, TXT_APELLIDO.Text, TXT_CEDULA.Text,
+                                   "", "", cbCargo.Text, TXT_TELEFONO.Text, fechaIngreso.ToString("yyyy-MM-dd"),
+                                   "", idDireccion.ToString(), idGenero.ToString());*/
+
                 MessageBox.Show("Empleado registrado correctamente");
             }
             catch (Exception ex)
             {
-                MessageBox.Show("No se pudo registrar el empleado" + ex);
+                MessageBox.Show("No se pudo registrar el empleado: " + ex.Message);
             }
         }
 
+        // Método para verificar si la dirección existe
+        /* private bool VerificarDireccionExistente(int Direccion)
+         {
+             // Aquí deberías hacer una consulta SQL para verificar si el ID de dirección existe en la base de datos
+             string query = "SELECT COUNT(*) FROM Direccion WHERE ID_DIRECCION = @id_Direccion";
+
+             using (SqlConnection conn = new SqlConnection("Server= RICARDO\\LOSER1; Database=BD_Empleados; Integrated Security=true"))
+             {
+                 SqlCommand cmd = new SqlCommand(query, conn);
+                 cmd.Parameters.AddWithValue("@idDireccion", Direccion);
+                 conn.Open();
+
+                 int count = (int)cmd.ExecuteScalar();
+                 return count > 0;  // Si count > 0, la dirección existe
+             }
+         } */
         private void TXT_EDAD_TextChanged(object sender, EventArgs e)
         {
 
@@ -44,7 +95,58 @@ public partial class FrmVisualregistrarempleado : Form
 
         private void FrmVisualregistrarempleado_Load(object sender, EventArgs e)
         {
+            string connectionString = "Server= RICARDO\\LOSER1; Database=BD_Empleados; Integrated Security=true";
+            string query = "select id_Direccion from Direccion";
 
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+
+                
+                {
+
+
+                    SqlCommand cmd = new SqlCommand(query, conn);
+                    conn.Open();
+                    SqlDataAdapter da = new SqlDataAdapter(cmd);
+                    DataTable dt = new DataTable();
+                    da.Fill(dt);
+
+                    cb_direccion.DisplayMember = "id_Direccion";
+                    cb_direccion.ValueMember = "id_Direccion";
+                    cb_direccion.DataSource = dt;
+                }
+
+              
+
+            }
         }
+
+        private void cb_direccion_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+            if (cb_direccion.SelectedIndex != null)
+            {
+                int iddireccion = Convert.ToInt32(cb_direccion.SelectedValue);
+             //   MessageBox.Show("El ID de la dirección seleccionada es: " + iddireccion);
+
+            }
+            //VerificarDireccionExistente1(1);
+        }
+            /*  // Método para verificar si la dirección existe
+        private bool VerificarDireccionExistente1(int Direccion)
+        {
+            // Aquí deberías hacer una consulta SQL para verificar si el ID de dirección existe en la base de datos
+            string query = "SELECT COUNT(*) FROM Direccion WHERE id_Direccion = @id_Direccion";
+
+            using (SqlConnection conn = new SqlConnection("Server= RICARDO\\LOSER1; Database=BD_Empleados; Integrated Security=true"))
+            {
+                SqlCommand cmd = new SqlCommand(query, conn);
+                cmd.Parameters.AddWithValue("@idDireccion", Direccion);
+                conn.Open();
+
+                int count = (int)cmd.ExecuteScalar();
+                return count > 0;  // Si count > 0, la dirección existe
+            }
+        }*/
     }
-}
+    }
